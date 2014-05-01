c1e505d2b13043bd40f59b353c6235063bf1c0cc
puhrome <phirom.yim@fullsail.edu>
2014-05-01 15:57:45 -0400

    Function_Worksheet_AreaRectangle
    Using Function to set up examples of how to calculate the area of a
    Rectangle

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/**
+ * Area of a Rectangle Function Created by Phirom Yim on 4/29/14.
+ */
+
+function calcArea(w, h){
+    console.log(w * h);
+}
+calcArea(20, 30);
+
+function calcArea(w, h){
+    console.log(w * h);
+}
+calcArea (4, 12);
